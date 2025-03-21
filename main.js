ba2c8ef967ba9bbcaef1f9d40823dace91a0dd3c
karina casas <casaskarina817@gmail.com>
2025-03-21 12:50:19 -0500

    feat: :sparkles: firts quiz of javascript

---
+++ main.js
@@ -0,0 +1,35 @@
+function expresiones((hola)[hola]{hola}){
+     if(true) {
+         let expresion =[hola];
+     }
+     console.log(expresion [hola]);
+ }   else (false){
+
+     let expresion = (hola);
+
+
+ }
+
+ function expresiones({hola},[hola],(hola)){
+     if(true) {
+        let expresion =[];
+     }
+     console.log(expresiones);
+ }
+
+ function expresiones({},[]){
+     if(true) {
+         let expresion =[];
+     }
+     console.log(expresiones);
+ }
+
+
+
+ let expresion = "[hola]";
+
+ switch(expresion) {
+     case "[hola]":
+         console.log("tiene corchete")
+         break;   
+ }
